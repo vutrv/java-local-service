01a679fd5087235a6656d24c33bc1c3a46c5e7ce
vutv <vutran.react.node.js@gmail.com>
2025-03-25 22:57:17 +0700

    Add minIO dto's fields

--- src/main/java/org/example/websocket/dtos/responses/MinIOCredentialsResponse.java
+++ src/main/java/org/example/websocket/dtos/responses/MinIOCredentialsResponse.java
@@ -1,4 +1,21 @@
 package org.example.websocket.dtos.responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MinIOCredentialsResponse {
+    private String endPoint;
+    private String accessKeyId;
+    private String secretKey;
+    private String sessionToken;
+    private Integer concurrentThreads;
+    private Integer maxConnections;
+    private Boolean allowMultipartUpload;
+    private String expiration;
 }
